e149f277b45b49e67467e6e4795560d8cf079312
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-04-17 14:51:10 -0400

    23

---
+++ grade.js
@@ -0,0 +1,13 @@
+let myGrade = 95
+
+if (myGrade >= 90) {
+    console.log("You have a A")
+  } else if (myGrade >= 80) {
+    console.log("You have a B")
+  } else if (myGrade >= 70) {
+    console.log("You have a C")
+  } else if (myGrade >= 60) {
+    console.log("You have a D")
+  } else {
+    console.log("You have a F")
+  }
---
+++ hello.js
@@ -0,0 +1,17 @@
+
+let myName = "Mekhi"
+let myAge = 16
+
+console.log("Hello my name is " + myName + ". My age is " + myAge)
+
+if (myAge>=16) {
+    console.log(myName + " is older than 16")
+} else {
+    console.log(myName + " is younger than 16")
+}
+
+let myList = ['Watermelon', 'Mango', 'Peach', 'Orange']
+
+for (let item of myList) {
+    console.log(item)
+}
