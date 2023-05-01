cdfad34744af7adcfb04c786b2d9c9fc7623a04d
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-05-01 14:03:59 -0400

    w

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,34 @@
-function {
-    alert("You have clicked the button.")
-}
+let timerDisplay = document.getElementsByClassName("timerdisplay")[0];
+let startTime, elapsedTime = 0, timerInterval;
+
+
+
+let displayElapsedTime = () => {
+  let minutes = Math.floor(elapsedTime / 60000);
+  let seconds = Math.floor((elapsedTime % 60000) / 1000);
+  let milliseconds = elapsedTime % 1000;
+  timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds.toString().slice(0, 2)}`;
+};  
+
+const startStopwatch = () => {
+  if (timerInterval) return;
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    displayElapsedTime();
+  }, 10);
+};
+
+const stopStopwatch = () => {
+  clearInterval(timerInterval);
+  timerInterval = null;
+};
+
+const resetStopwatch = () => {
+  clearInterval(timerInterval);
+  timerInterval = null;
+  elapsedTime = 0;
+  displayElapsedTime();
+};
+
+
